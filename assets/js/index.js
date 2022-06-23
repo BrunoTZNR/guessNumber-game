543832032b9573f91045bbbf0d93c92c606abffb
Bruno Marco <brunomarcodasilva@gmail.com>
2022-06-23 02:01:32 -0300

    update design, html, css, js and add mit license

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,26 @@
-const numAleatorio = Math.floor(Math.random() * (100 + 1)); //gera o número a ser adivinhado
-// console.log(numAleatorio);
+const numAleatorio = Math.floor(Math.random() * (100 - 1) + 1); //gera o número a ser adivinhado
+console.log(numAleatorio);
 
-var numeros = [];
+var j = 0;
+var numeros = 0;
 var tentativas = 0;
+
+//limpar input
+const limpa = () => {
+    document.querySelector('.input-number').value = "";
+    document.querySelector('.input-number').focus();
+}
 
 //imprime o numeros já digitados
 const respostas = () => {
-    const res = document.querySelector('.res');
-    res.innerHTML = numeros.join('-');
+    var teste = `#td${numeros}`;
+    var tabela=document.querySelector(teste);
+    
+    if(j===1){
+        tabela.classList.add('acertado');
+    }else{
+        tabela.classList.add('acerto');
+    }
 }
 
 //tocar um audio aleatorio de 1 a 10
@@ -21,45 +34,52 @@ const music = () => {
 }
 
 const consequencia = () => {
-    const checkbox = document.querySelector('#dica-checkbox').checked;
-    const dica = document.querySelector('.dica-palavra');
+    const checkbox = document.querySelector('#dica-checkbox');
     const res = document.querySelector('.res');
+    const dica = document.querySelector('.dica-palavra');
     const input = document.querySelector('.input-number').value;
 
-    if(+input > 100 || +input < 0){
-        res.innerHTML = "Digite um valor entre 0 e 100!";
-        document.querySelector('.input-number').value = "";
-        document.querySelector('.input-number').focus();
+    if(+input > 100 || +input < 0 || input == 0){
+        res.innerHTML = "Digite um valor entre 1 e 100!";
     }else{
-        if(checkbox == true){
-            if(+input == numAleatorio){
-                music(); //tocar a musica caso ganhar
-            }else if(+input < numAleatorio){
+        if(checkbox.checked == true){
+            document.querySelector('.dica-sinal').classList.add('corzinha');
+            checkbox.disabled = true;
+
+            if(+input < numAleatorio){
                 dica.innerHTML = "O número é maior!";
-                numeros.push(input);
-                respostas();
+                numeros=input;
+                // respostas();
             }else if(+input > numAleatorio){
                 dica.innerHTML = "O número é menor!";
-                numeros.push(input);
-                respostas();
-            }
-        }else{
-            if(+input === numAleatorio){
-                music();
-            }else{
-                numeros.push(input);
-                respostas();
+                numeros=input;
+                // respostas();
             }
         }
-        document.querySelector('.input-number').value = "";
-        document.querySelector('.input-number').focus();
+
+        if(+input === numAleatorio){
+            document.querySelector('.input-number').disabled = true;
+            document.querySelector('.verificar').disabled = true;
+
+            numeros=input;
+            j=1;
+
+            checkbox.disabled = true;
+
+            music();
+        }else{
+            numeros=input;
+
+            limpa();
+        }
         tentativas += 1;
+        respostas();
     }
 }
 
 //dispara consequencia apertando enter ao inves de clickar no botao
 addEventListener('keypress', function(e){
     if(e.key==='Enter'){
-        consequencia()
+        consequencia();
     }
 })
